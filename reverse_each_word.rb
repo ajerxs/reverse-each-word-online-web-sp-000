8310e02383a4f0b5bd795ea4e3ae23d5ee178566
Andrew Eames <ajerxs@gmail.com>
2020-11-08 23:49:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence1)
-  array = sentence1.split(" ")
+  reversed_sentence = sentence1.reverse
+  array = reversed_sentence.split(" ")
   array.each do |words|
     "#{words}"
   end
-  array.reverse
 end
