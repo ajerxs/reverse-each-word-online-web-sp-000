b6d3ff1c8322ccc50020276c1485d71f3b59de40
Andrew Eames <ajerxs@gmail.com>
2020-11-08 23:40:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence1)
-  sentence1.reverse
-  sentence1.split(" ")
+  reversed_sentence = sentence1.reverse
+  reversed_array = reversed_sentence.split(" ")
+  reversed_sentence1 = reversed_array.reverse
 end
